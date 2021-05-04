9e73761e99b1787cbb7db5443bd85a9674204b18
Rose <qqbunny@icloud.com>
2021-05-04 17:18:48 +0800

    Initial Commit

---
+++ TaxEasyCalculator小費計算機/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TaxEasyCalculator小費計算機
+//
+//  Created by Rose on 2021/5/4.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
